c13057c13bfd94d186d2f21ae9b4fd737e49af00
wdower <will@dower.dev>
2023-10-18 11:14:43 -0400

    creating a local profile to make managing dependencies easier
    Signed-off-by: wdower <will@dower.dev>

---
+++ pipeline/pipeline_profile/controls/overlay.rb
@@ -0,0 +1,4 @@
+# This simple overlay does nothing other than run the controls
+# included in the Ubuntu 20.04 STIG baseline.  If we wanted to,
+# we could any of those controls in this file.
+include_controls 'ubuntu-20.04-stig-baseline'
